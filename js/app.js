9cdb9eddcafbab16a1477350d9591a062b6a6742
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-03-05 00:45:47 +0000

    AutomatedCommitAndPush

---
+++ js/app.js
@@ -0,0 +1,14 @@
+alert("exam1")
+/*
+$.ajax({
+    method: "GET",
+    url: url,
+    dataType: "json",
+    success: function(result, status){
+        //alert("success");
+    },
+    error: function(status, error){
+        alert("failure");
+    }
+});
+*/
