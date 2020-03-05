80f0aacc9cb054fd0ab71cbc49c079e4e84869ac
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-03-05 02:44:11 +0000

    AutomatedCommitAndPush

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
-alert("exam1")
-/*
-$.ajax({
-    method: "GET",
-    url: url,
-    dataType: "json",
-    success: function(result, status){
-        //alert("success");
-    },
-    error: function(status, error){
-        alert("failure");
-    }
-});
-*/
+
+// https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&format=json&jscmd=data
+//var baseUrl = https://openlibrary.org/api/books?
+
+console.log(getData("0451526538"));
+
+
+function getData(isbn){
+    var baseUrl = "https://openlibrary.org/api/books?"
+    var urlToUse = baseUrl + "bibkeys=ISBN" + isbn + "&format=json&jscmd=data";
+    $.ajax({
+        method: "GET",
+        url: baseUrl,
+        dataType: "json",
+        success: function(result, status){
+            //alert("success");
+            return result;
+        },
+        error: function(status, error){
+            alert("failure");
+        }
+    });
+}
